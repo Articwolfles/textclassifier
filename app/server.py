a1ce1e620256ba4e7b781918cc97d997c14d97d4
Articwolfles <58208257+Articwolfles@users.noreply.github.com>
2019-11-28 10:22:31 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -57,10 +57,10 @@ async def homepage(request):
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
     req = await request.form()
-    input_text= req["input-text"]
+    input_text= req["""input-text"""]
 
     prediction = learn.predict(input_text)[0]
-    return JSONResponse({'The genre is': str(prediction)})
+    return JSONResponse({'The genre is' + str(prediction)})
 
 
 if __name__ == '__main__':
